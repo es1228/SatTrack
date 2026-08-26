da35c1ae0065fb69a956f6b6fbf857e8d3255d17
Evan Sidhu <129010227+es1228@users.noreply.github.com>
2026-08-25 23:40:31 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import World from "./templates/World";
+
+function App() {
+
+	return (
+		<div className="h-screen w-screen">
+			<World />
+		</div>
+	);
+}
+
+export default App;
